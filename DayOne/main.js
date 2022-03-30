be8a0f48451a8b704a5f44a2a08e9dd393243be6
thinkwithabs <programmerabs99@gmail.com>
2022-03-30 17:16:07 +0600

    Day 2end

--- DayOne/main.js
+++ DayOne/main.js
@@ -1,1 +1,71 @@
-consle.log("First Code");
+// // variable : var , let , const 
+
+// // if(true){
+// //     var lname = 'xyz'; //var - globally accs
+// //     let mname = "abc";
+// //     const fname = "abc xyz";
+// //  }
+// // if(true){
+// //     console.log(fname);
+// // }
+// // console.log(fname);
+// // // console.log(mname);
+// // // console.log(lname);
+// // // console.log(fname);
+
+// // var name = "abc"; //5kb
+// // var name ="xyz"; //5kb
+// // name= "hello";
+
+// // let name ="abc";
+// // // let name = "xyz";
+// // name ="hello";
+// // name ="hello";
+
+// // const name ="abc";
+// // // const name ="xyz";
+// // // name ="hello";
+
+// const dbname = "fsdjalkf";
+// const dbuser = 'root';
+// const dbpass = "456465fsda"
+
+// console.log(name);
+
+
+// Data Type in Js 
+// string , number , Boolean , array , object,  undefined, null;
+
+
+// let fullname = "Abu bakkar siddik";
+// let age = 22.5145;
+// let married = false;
+// let skills = ['html',5464,true,'javascript'];
+
+// let aboutme = {
+//     'fullname':'abs shakil',
+//     'age': 156,
+//     'skills': ['html',5464,true,'javascript'],
+//     'myfun':function(){
+//         console.log("I am writing");
+//     }
+// }
+
+// let address;
+// let add = null;
+// console.log(add);
+
+
+// function MyFun(name,time,desig,endtime){
+//     console.log(`Hello Good ${time} I am ${name} , I'm ${desig}`);
+//     return "xyz";
+//     console.log(`Nice to Meet you Good ${endtime}`);
+// }
+// MyFun('muhit','Afternoon','Programmer',"Night");
+
+function calculator(x,y){
+    return x + y ;
+}
+ 
+console.log(calculator(10,5));
+
